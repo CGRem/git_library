a84e7db33fafdfd0095703f0d1f71658311a4da6
Rem <cgrem78@yandex.ru>
2026-03-06 16:11:46 +0300

    function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstdlib> // для system
+#include "function.h"
 using namespace std;
 
 int main()
 {
     cout << "Hello, world!" << endl;
-    system("pause"); // Только для тех, у кого MS Visual Studio
+    //system("pause"); // Только для тех, у кого MS Visual Studio
     return 0;
 }
