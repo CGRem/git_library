117dd637964756bea21f1bfe33f6394fb32adf79
Rem <cgrem78@yandex.ru>
2026-03-17 17:40:22 +0300

    string dynamic

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <cstdlib> // для system
+#include <string>
 #include "function.h"
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "");
-    cout << "Hello, world!" << endl;
-    cout << func_r::summ_r(2, 5) << endl;
+    string des_u;
+    string des_sum;
+    des_u = "cst_g ---";
+    for (int i = 0; i < 80; i++) {
+
+        des_u = des_u + '*';
+        //system("cls");
+        cout << des_u << endl;
+    }
+    for (int i = 0; i < 80; i++) {
+
+        des_u = des_u.erase();
+        //system("cls");
+        cout << des_u << endl;
+    }
     //system("pause"); // Только для тех, у кого MS Visual Studio
     return 0;
 }
